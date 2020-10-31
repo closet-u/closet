794b7e578e4bf364a71156bdbc0b273c50b66c57
mannynunez8 <53127043+mannynunez8@users.noreply.github.com>
2020-10-31 17:21:27 -0500

    Update backend.py

--- back-end/backend.py
+++ back-end/backend.py
@@ -1,23 +1,58 @@
-from pymongo import MongoClient
-import json
-from login import *
+import urllib.parse
+import flask
+from flask import Flask, request
 from utils import *
+from backend import *
 
-#DBConfiguration
-#client = pymongo.MongoClient("mongodb+srv://mannynunez8:12345@cluster0.pviz6.gcp.mongodb.net/test?retryWrites=true&w=majority")
-client = MongoClient("mongodb://127.0.0.1", 27017)
+#from flask_cors import CORS, cross_origin
+#Flask setup
+app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS']= 'Content-Type'
 
-#establish DB and collections
-db = client.closetU
-loginCol = db.login
-print("connected")
+#registration path
+@app.route("/register")
+def registration(body):
+    body = request.body
+    info = json.loads(body)
+    print(info.keys())
+    result = save_login(body)
 
-#test input data into DB
-#post = {"username": "mannynu", "password": "12345"}
-#post = login_info()
-print("server info: ", client.server_info())
+    #if registration information is valid- instert in # DB
+    if result == True:
+        loginCol.insertOne(body)
+        print("Registration complete")
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    else:
+        return json.dumps({'Fail':False}), 400, {'ContentType':'application/json'}
 
-#insert data into DB
+#login path
+@app.route("/login")
+def login(body):
+    body = request.body
+    info = json.loads(body)
+    print(info.keys())
+    result = validate(body)
+    #if login information is valid- return True
+    if result == True:
+        post = body
+        print("Login Successful!")
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    elif result == False:
+        print("Username or Password is Incorrect")
+    else:
+        return json.dumps({'Fail':False}), 400, {'ContentType':'application/json'}
 
-#loginCol.insert(post)
-print("complete")
+def main():
+    post = {'username':'claudia1','password':'54321'}
+    registration(post)
+    print("main")
+    # login(post)
+
+main()
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
+
