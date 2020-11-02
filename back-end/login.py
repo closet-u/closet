8e6a13e01bed221db63c71a6cb486dabbbbb12ce
mannynunez8 <53127043+mannynunez8@users.noreply.github.com>
2020-11-01 19:55:35 -0600

    Update login.py

--- back-end/login.py
+++ back-end/login.py
@@ -1,34 +1,34 @@
 import urllib.parse
-import request
 import flask
-from flask import Flask
-from utils import save_login
+from flask import Flask, request
+from utils import *
+from backend import *
 
 #from flask_cors import CORS, cross_origin
 #Flask setup
-
 app = Flask(__name__)
 # cors = CORS(app)
 # app.config['CORS_HEADERS']= 'Content-Type'
 
 #registration path
 @app.route("/register")
-def registration():
+def registration(body):
     body = request.body
     info = json.loads(body)
     print(info.keys())
     result = save_login(body)
+
     #if registration information is valid- instert in # DB
     if result == True:
-        post = body
-        loginCol.insert(post)
+        loginCol.insertOne(body)
+        print("Registration complete")
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     else:
         return json.dumps({'Fail':False}), 400, {'ContentType':'application/json'}
 
 #login path
 @app.route("/login")
-def login():
+def login(body):
     body = request.body
     info = json.loads(body)
     print(info.keys())
@@ -38,13 +38,20 @@ def login():
         post = body
         print("Login Successful!")
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-    else if result == False:
+    elif result == False:
         print("Username or Password is Incorrect")
     else:
         return json.dumps({'Fail':False}), 400, {'ContentType':'application/json'}
 
+def main():
+    post = {'username':'claudia1','password':'54321'}
+    registration(post)
+    print("main")
+    # login(post)
+
+main()
 
 
 
-if __name__ == '__main__':
-    app.run(debug = True)
+# if __name__ == '__main__':
+#     app.run(debug = True)
