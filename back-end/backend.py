e8b9849103940ba132629ed1054027e59687b691
mannynunez8git <mannynu2014@gmail.com>
2020-11-01 21:20:11 -0600

    lol

--- back-end/backend.py
+++ back-end/backend.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 import json
 
 # DBConfiguration
-client = pymongo.MongoClient(
-    "mongodb+srv://mannynunez8:12345@cluster0.pviz6.gcp.mongodb.net/test?retryWrites=true&w=majority")
-#client = MongoClient("mongodb://127.0.0.1", 27017)
+# client = pymongo.MongoClient(
+#     "mongodb+srv://mannynunez8:12345@cluster0.pviz6.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb://127.0.0.1", 27017)
 
 # establish DB and collections
 db = client.closetU
